daaab9ad4a6a3a388c74fa92486c40ecc52b7bf7
Luca <luca.rengo@studio.unibo.it>
2021-11-20 23:45:48 +0100

    Add homepage.fxml and HomePageController Class.

---
+++ src/main/java/com/lucar01/aeroporto/HomePageController.java
@@ -0,0 +1,37 @@
+package com.lucar01.aeroporto;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class HomePageController {
+
+    @FXML
+    private Button btnAddTable;
+
+    @FXML
+    private Button btnAlterTable;
+
+    @FXML
+    private Button btnOverview;
+
+    @FXML
+    private Button btnQuit;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    private Pane paneStatus;
+
+    @FXML
+    private Label paneStatusSubtitle;
+
+    @FXML
+    private Label paneStatusTitle;
+
+}
