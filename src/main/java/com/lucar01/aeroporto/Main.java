df1893cc2e1500c03f9b5cb9abfeadc88ce85769
Luca <luca.rengo@studio.unibo.it>
2021-11-20 23:44:58 +0100

    Update and renamed HelloApplication in Main.

--- src/main/java/com/lucar01/aeroporto/Main.java
+++ src/main/java/com/lucar01/aeroporto/Main.java
@@ -4,15 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("homepage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load()); // 320, 240
+		stage.initStyle(StageStyle.UNDECORATED); // così non mette il bordo di default con la chiusura, ecc..
+        stage.setTitle("Aeroporto");
         stage.setScene(scene);
         stage.show();
     }
