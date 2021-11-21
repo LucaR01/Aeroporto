f51a5446220d88137b854549eead41ff3ed9434b
Luca <luca.rengo@studio.unibo.it>
2021-11-21 21:50:57 +0100

    Update Main to have draggable user interface.

--- src/main/java/com/lucar01/aeroporto/Main.java
+++ src/main/java/com/lucar01/aeroporto/Main.java
@@ -1,18 +1,36 @@
 package com.lucar01.aeroporto;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private double offsetX = 0;
+    private double offsetY = 0;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("homepage.fxml"));
         Scene scene = new Scene(fxmlLoader.load()); // 320, 240
+
+        scene.setOnMousePressed(event -> {
+            offsetX = event.getSceneX();
+            offsetY = event.getSceneY();
+        });
+
+        scene.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - offsetX);
+            stage.setY(event.getScreenY() - offsetY);
+        });
+
+
 		stage.initStyle(StageStyle.UNDECORATED); // così non mette il bordo di default con la chiusura, ecc..
         stage.setTitle("Aeroporto");
         stage.setScene(scene);
