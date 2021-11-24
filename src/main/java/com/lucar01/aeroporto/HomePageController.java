c3f62f08b7fb0caa8a7bfe6b1b712962c5409a54
Luca <luca.rengo@studio.unibo.it>
2021-11-24 23:04:34 +0100

    Add combo box and its relative handleTableSelection() method.

--- src/main/java/com/lucar01/aeroporto/HomePageController.java
+++ src/main/java/com/lucar01/aeroporto/HomePageController.java
@@ -1,14 +1,15 @@
 package com.lucar01.aeroporto;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -66,38 +67,51 @@ public class HomePageController implements Initializable {
     @FXML
     private GridPane paneSettings;
 
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private TableColumn<?, ?> col_id;
+
+    @FXML
+    private ComboBox<String> combo_tables;
+
+    @FXML
+    private Button btnSelectedTable;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        ObservableList<String> tablesList = FXCollections.observableArrayList("Persona", "Aereo"); //TODO: li prendo da un enum tables del database
+        this.combo_tables.setItems(tablesList);
     }
 
     @FXML
     private void handleButtonsClick(ActionEvent event){ //TODO: rename in handleMenuButtons?
         //TODO: try to use a switch if possible
-        if(event.getSource() == btnOverview){
-            lblStatusTitle.setText("Overview");
-            lblStatusSubtitle.setText("Visualizza le tabelle");
-            paneStatus.setBackground(new Background(new BackgroundFill(Color.rgb(18, 162, 237), CornerRadii.EMPTY, Insets.EMPTY)));
-            paneOverview.toFront();
+        if(event.getSource() == this.btnOverview){
+            this.lblStatusTitle.setText("Overview");
+            this.lblStatusSubtitle.setText("Visualizza le tabelle");
+            this.paneStatus.setBackground(new Background(new BackgroundFill(Color.rgb(18, 162, 237), CornerRadii.EMPTY, Insets.EMPTY)));
+            this.paneOverview.toFront();
         }
-        else if(event.getSource() == btnAddTable){
-            lblStatusTitle.setText("Add Table");
-            lblStatusSubtitle.setText("Aggiungi tabelle al database");
-            paneStatus.setBackground(new Background(new BackgroundFill(Color.rgb(123, 20, 23), CornerRadii.EMPTY, Insets.EMPTY)));
-            paneAddTable.toFront();
+        else if(event.getSource() == this.btnAddTable){
+            this.lblStatusTitle.setText("Add Table");
+            this.lblStatusSubtitle.setText("Aggiungi tabelle al database");
+            this.paneStatus.setBackground(new Background(new BackgroundFill(Color.rgb(123, 20, 23), CornerRadii.EMPTY, Insets.EMPTY)));
+            this.paneAddTable.toFront();
         }
-        else if(event.getSource() == btnAlterTable){
-            lblStatusTitle.setText("Alter Table");
-            lblStatusSubtitle.setText("Modifica le tabelle");
-            paneStatus.setBackground(new Background(new BackgroundFill(Color.rgb(14, 64, 28), CornerRadii.EMPTY, Insets.EMPTY))); // 44, 125, 51
-            paneAlterTable.toFront();
+        else if(event.getSource() == this.btnAlterTable){
+            this.lblStatusTitle.setText("Alter Table");
+            this.lblStatusSubtitle.setText("Modifica le tabelle");
+            this.paneStatus.setBackground(new Background(new BackgroundFill(Color.rgb(14, 64, 28), CornerRadii.EMPTY, Insets.EMPTY))); // 44, 125, 51
+            this.paneAlterTable.toFront();
         }
-        else if(event.getSource() == btnSettings){
-            lblStatusTitle.setText("Settings");
-            lblStatusSubtitle.setText("Modifica le tue impostazioni");
-            paneStatus.setBackground(new Background(new BackgroundFill(Color.rgb(56, 11, 90), CornerRadii.EMPTY, Insets.EMPTY))); // 109, 21, 170
-            paneSettings.toFront();
+        else if(event.getSource() == this.btnSettings){
+            this.lblStatusTitle.setText("Settings");
+            this.lblStatusSubtitle.setText("Modifica le tue impostazioni");
+            this.paneStatus.setBackground(new Background(new BackgroundFill(Color.rgb(56, 11, 90), CornerRadii.EMPTY, Insets.EMPTY))); // 109, 21, 170
+            this.paneSettings.toFront();
         } else {
             if(event.getSource() == btnQuit){
                 // TODO: message dialog : Are you sure to exit? Yes or No
@@ -128,5 +142,12 @@ public class HomePageController implements Initializable {
     private void handleMinimizeButton(MouseEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setIconified(true);
+    }
+
+    @FXML
+    void handleTableSelection(ActionEvent event) { //TODO: or just call it selectTables?
+        //TODO: quando seleziona una tabella diversa, deve cambiare la tabella nella schermata di table, deve mostrare i dati della tabella nelle colonne.
+        String selectedTable = this.combo_tables.getSelectionModel().getSelectedItem().toString();
+        this.btnSelectedTable.setText(selectedTable);
     }
 }
