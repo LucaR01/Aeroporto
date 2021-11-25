798341bacae1742c73b1ca5d2ab8f36e7c970cbc
Luca <luca.rengo@studio.unibo.it>
2021-11-25 21:45:02 +0100

    Add mysql requirements.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,18 @@
 module com.lucar01.aeroporto {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires validatorfx;
     requires org.kordamp.ikonli.javafx;
+    requires mysql.connector.java;
 
     opens com.lucar01.aeroporto to javafx.fxml;
+    // opens controllers;
+    // opens table;
     exports com.lucar01.aeroporto;
+    exports com.lucar01.aeroporto.table;
+    opens com.lucar01.aeroporto.table to javafx.fxml;
 }
