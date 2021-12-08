64f0ab3f2c8861e7894ddfda50290b08b338a953
Luca <luca.rengo@studio.unibo.it>
2021-12-08 19:16:09 +0100

    Add junit to the project.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,11 +7,17 @@ module com.lucar01.aeroporto {
     requires com.dlsc.formsfx;
     requires validatorfx;
     requires org.kordamp.ikonli.javafx;
+
     requires mysql.connector.java;
+
+    requires junit;
+    requires org.junit.jupiter.api;
 
     opens com.lucar01.aeroporto to javafx.fxml;
     // opens controllers;
     // opens table;
+    opens test.com.lucar01.aeroporto;
+
     exports com.lucar01.aeroporto;
     exports com.lucar01.aeroporto.table;
     opens com.lucar01.aeroporto.table to javafx.fxml;
